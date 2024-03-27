8a3ed8838837e3bd7336b614311ffc657e6709b9
Adel <snakeadel00@gmail.com>
2024-03-27 08:57:39 +0300

    add adaptive to main page

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -5,6 +5,21 @@ const config = {
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  components: {
+    Text: {
+      baseStyle: {
+        fontSize: {
+          base: '16px',
+          sm: '16px',
+          md: '16px',
+          lg: '20px',
+          xl: '24px',
+        },
+      },
+    },
+  },
+});
 
 export default theme;
--- src/widgets/HomeContent/ui/index.tsx
+++ src/widgets/HomeContent/ui/index.tsx
@@ -8,7 +8,7 @@ import { cardItems } from '../constants/CardItems';
 
 export const HomeContent = () => (
   <Flex direction={'column'}>
-    <Heading as="h1" textAlign="center" mb="100px">
+    <Heading as="h1" textAlign="center" mb={{base: '32px', md: '64px', lg: '100px'}}>
       Стек технологий
     </Heading>
     <Flex direction={'column'} gap={'100px'}>
